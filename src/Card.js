ec880e6f24323c916640c064f8c14dca73499460
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 22:24:21 -0500

    Even More Card Changes

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,8 @@ const Card = ({ color, title }) => {
   return (
     <div className="card mx-3" style={{ backgroundColor: color }}>
       <div className="card-body">
-        <h5 className="card-title text-light">Card {title}</h5>
+        <h5 className="card-title text-light mb-3">Card {title}</h5>
+        <hr className="text-light" style={{opacity: '0.3'}} />
         <p className="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
     </div>
