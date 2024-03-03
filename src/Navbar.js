fc980f32f634e253c54a1c19a4eebab9845f0803
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 21:31:16 -0500

    Changes to Navbar 3

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,17 +4,27 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <ul className="navbar-nav">
-          <li className="nav-item" style={{ display: 'inline-block', marginRight: '10px' }}>
+        {/* Logo */}
+        <a className="navbar-brand" href="#">Navbar</a>
+
+        {/* Links */}
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
             <a className="nav-link" href="#">Home</a>
           </li>
-          <li className="nav-item" style={{ display: 'inline-block', marginRight: '10px' }}>
+          <li className="nav-item">
             <a className="nav-link" href="#">About</a>
           </li>
-          <li className="nav-item" style={{ display: 'inline-block' }}>
+          <li className="nav-item">
             <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
+
+        {/* Search Bar */}
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
       </div>
     </nav>
   );
