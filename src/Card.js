4928754846dc4c3eda144706067c6463bc08a6ec
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 22:35:58 -0500

    More Changes

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = ({ color, title }) => {
   return (
     <div className="card mx-3" style={{ backgroundColor: color }}>
       <div className="card-body">
-        <h5 className="card-title text-light mb-3">Card {title}</h5>
+        <h2 className="card-title text-light mb-3">Card {title}</h2>
         <hr className="text-light" style={{opacity: '0.3'}} />
         <p className="card-text text-light">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,22 +3,25 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid d-flex justify-content-between">
-        {/* Logo */}
-        <a className="navbar-brand" href="#">Navbar</a>
+      <div className="container-fluid">
+        {/* Logo and Links */}
+        <div className="d-flex">
+          {/* Logo */}
+          <a className="navbar-brand" href="#">Navbar</a>
 
-        {/* Links */}
-        <ul className="navbar-nav d-flex flex-row">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
+          {/* Links */}
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
 
         {/* Search Bar */}
         <form className="d-flex">
