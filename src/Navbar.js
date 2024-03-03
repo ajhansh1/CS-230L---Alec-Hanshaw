91a57fe2cf46ee613c61a2d7c82ca0e48b1f0a23
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 21:28:58 -0500

    Slight Changes to Navbar 2

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,14 +4,14 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
+        <ul className="navbar-nav">
+          <li className="nav-item" style={{ display: 'inline-block', marginRight: '10px' }}>
             <a className="nav-link" href="#">Home</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" style={{ display: 'inline-block', marginRight: '10px' }}>
             <a className="nav-link" href="#">About</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" style={{ display: 'inline-block' }}>
             <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
