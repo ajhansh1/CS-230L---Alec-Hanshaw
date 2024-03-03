fcaf3db913fd9035e3af473727feeaa4edaffc54
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 20:16:12 -0500

    AWS 2 Changes

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Card = () => {
   return (
     <div className="card">
-      <h2>Card Title</h2>
-      <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
     </div>
   );
 }
