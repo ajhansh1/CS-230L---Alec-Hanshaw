98057addb5fe8bc9b46433613b0dd8c6a545c3d1
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 20:30:39 -0500

    Changes 4

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
+      <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+        <li style={{ marginRight: '10px' }}><a href="#">Home</a></li>
+        <li style={{ marginRight: '10px' }}><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
     </nav>
