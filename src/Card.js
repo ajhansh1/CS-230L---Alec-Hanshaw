9b7fc1b4cbdc40accb1184a07f54012eb743558c
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 20:19:19 -0500

    Changes 2

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ color }) => {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: color }}>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
