c7ae37ed6ced8c23d4ff04caa0c689c059b75120
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 21:33:06 -0500

    Slight Changes to Navbar 4

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
+      <div className="container d-flex justify-content-between align-items-center">
         {/* Logo */}
         <a className="navbar-brand" href="#">Navbar</a>
 
         {/* Links */}
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav mr-auto d-flex flex-row">
           <li className="nav-item">
             <a className="nav-link" href="#">Home</a>
           </li>
@@ -21,7 +21,7 @@ const Navbar = () => {
         </ul>
 
         {/* Search Bar */}
-        <form className="form-inline my-2 my-lg-0">
+        <form className="form-inline">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
