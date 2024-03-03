abfa1071629e6b7ee4074afe534f99b76fde6627
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 20:20:51 -0500

    Changes 3

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 02</h2>
       <p>WVU ID: 800373257</p>
       <p>Hi, I am Alec Hanshaw.</p>
       <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
+      <div className="row">
+          <div className="col">
+            <Card color="red" />
+          </div>
+          <div className="col">
+            <Card color="green" />
+          </div>
+          <div className="col">
+            <Card color="blue" />
+          </div>
+        </div>
       </div>
     </div>
   );
