36195372e4977e3010c28cb07303cb87b9153a8d
ajhansh1 <ajhansh1@yahoo.com>
2024-03-02 21:21:20 -0500

    Changes 5

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
-        <li style={{ marginRight: '10px' }}><a href="#">Home</a></li>
-        <li style={{ marginRight: '10px' }}><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
